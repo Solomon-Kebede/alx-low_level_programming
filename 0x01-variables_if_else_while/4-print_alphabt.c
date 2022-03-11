f39700f4cc56e69730a085d49a17a9715317fe9e
root <root@localhost>
2022-03-11 11:48:23 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print lower case characters of alphabet except q & e
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		if (i != 113 && i != 101)
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
