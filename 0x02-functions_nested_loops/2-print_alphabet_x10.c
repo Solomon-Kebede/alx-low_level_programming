424b6cbdb05dcb779270bce960aa4b50d7c6bf7b
root <root@localhost>
2022-03-15 13:36:22 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lower case characters 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+	_putchar('\n');
+	}
+
+}
