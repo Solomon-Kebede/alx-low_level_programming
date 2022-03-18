3851965b62df1400d99c252cfc601753373da5de
root <root@localhost>
2022-03-18 11:15:36 -0700

    UPDATE 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
-# include "main.h"
+#include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
 
 void print_numbers(void)
 {
-        int i;
+	int c;
 
-        for (i = 0; i < 10; i++)
+	for (c = 0; c < 10; c++)
 	{
-		_putchar(i + '0');
+		_putchar(c + '0');
 	}
 
-        _putchar('\n');
+	_putchar('\n');
 }
