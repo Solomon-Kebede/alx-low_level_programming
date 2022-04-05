daec92fb2e6a8f5247119154f0b9f4e63c77906e
root <root@localhost>
2022-04-05 04:27:50 -0700

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,22 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
-	int i;
-	int sl1 = strlen(s1);
-	int sl2 = strlen(s2);
+	int i, sl1, sl2;
+
+	if (s1 == NULL)
+	{
+		s1 = '\0';
+		sl1 = 0;
+	}
+	else
+		sl1 = strlen(s1);
+	if (s2 == NULL)
+	{
+		s2 = '\0';
+		sl2 = 0;
+	}
+	else
+		sl2 = strlen(s2);
 
 	/* allocate for '\0' once */
 	str = malloc(sizeof(char) * (sl1 + sl2 + 1));
