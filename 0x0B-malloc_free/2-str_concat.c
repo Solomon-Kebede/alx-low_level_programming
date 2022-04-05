3a11ee130c120eaec3a734133a4ee20c2fa83a94
root <root@localhost>
2022-04-05 04:39:13 -0700

    Update-2 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,12 +33,17 @@ char *str_concat(char *s1, char *s2)
 	/* allocate for '\0' once */
 	str = malloc(sizeof(char) * (sl1 + sl2 + 1));
 
-	for (i = 0; i < sl1; i++)
-		str[i] = s1[i];
+	if (str)
+	{
+		for (i = 0; i < sl1; i++)
+			str[i] = s1[i];
 
-	for (i = 0; i < sl2; i++)
-		str[i + sl1] = s2[i];
+		for (i = 0; i < sl2; i++)
+			str[i + sl1] = s2[i];
 
-	str[sl1 + sl2] = '\0';
-	return (str);
+		str[sl1 + sl2] = '\0';
+		return (str);
+	}
+	else
+		return (0);
 }
