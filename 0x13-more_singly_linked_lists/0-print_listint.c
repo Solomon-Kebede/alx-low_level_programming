aa0b04cff8b194145e75bd8cf78d4d7b2afcc385
root <root@localhost>
2022-04-26 06:31:58 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - print list
+ * @h: pointer
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
