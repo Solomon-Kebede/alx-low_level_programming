3af05ace89687bb386a376282baed641421d7f31
root <root@localhost>
2022-04-26 08:12:36 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -23,5 +23,6 @@ void free_listint(listint_t *head); /*recheck */
 void free_listint2(listint_t **head); /* recheck */
 int pop_listint(listint_t **head); /* double recheck */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
 
 #endif
