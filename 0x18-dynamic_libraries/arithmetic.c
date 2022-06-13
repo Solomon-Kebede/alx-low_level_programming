a085335e6288973e5d814c5bf9a90f0e5e31e132
root <root@localhost>
2022-06-13 13:26:56 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/arithmetic.c
@@ -0,0 +1,72 @@
+/**
+ * add - adds x and y
+ * @x: integer
+ * @y: integer
+ * Return: sum
+ */
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
+
+/**
+ * sub - subtracts x from y
+ * @x: integer
+ * @y: integer
+ * Return: difference
+ */
+
+int sub(int x, int y)
+{
+	int diff;
+
+	diff = x - y;
+	return (diff);
+}
+
+/**
+ * mul - multiply x and y
+ * @x: integer
+ * @y: integer
+ * Return: product
+ */
+
+int mul(int x, int y)
+{
+	int product;
+
+	product = x * y;
+	return (product);
+}
+/**
+ * div - divide x by y
+ * @x: integer
+ * @y: integer
+ * Return: quotient
+ */
+
+int div(int x, int y)
+{
+	int quotient;
+
+	quotient = x / y;
+	return (quotient);
+}
+/**
+ * mod - mods x by y
+ * @x: integer
+ * @y: integer
+ * Return: modulus
+ */
+
+int mod(int x, int y)
+{
+	int modulus;
+
+	modulus = x % y;
+	return (modulus);
+}
