866ab4cb88c0db75ed231da3a72569e14f235a34
Solomnon Kebede <solomnonaau188@gmail.com>
2023-04-13 11:08:08 +0000

    Task 0. >>> ht = {}: (create)

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size) {
+	/* declare a new hashtable */
+	hash_table_t *new_array;
+
+	new_array = malloc(size * sizeof(hash_table_t));
+	if (new_array == NULL)
+		return (NULL);
+
+	return (new_array);
+}
