3e80b0a2721b7da3f0b395c49e8235cf386d5381
Solomon Kebede <solomonaau188@gmail.com>
2023-04-13 17:36:26 +0000

    Task 0 - free memory

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -25,7 +25,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_array->size = size;
 	new_array->array = malloc(size * sizeof(hash_node_t));
 	if (!new_array->array)
+	{
+		/* free memory */
+		free(new_array);
 		return (NULL);
+	}
 
 	return (new_array);
 }
