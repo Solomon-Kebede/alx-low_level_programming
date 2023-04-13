e8f1cc23ddd07607a127874ea31278629da52b2e
Solomon Kebede <solomonaau188@gmail.com>
2023-04-13 21:46:41 +0000

    Task 3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,6 +8,7 @@
  * @value: is the value associated with the key
  * Return: 1 if it succeeded, 0 otherwise
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
