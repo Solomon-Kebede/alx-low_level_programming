942fa0c493d958a354a8c2e6dd09a8816b9148e3
Solomon Kebede <solomonaau188@gmail.com>
2023-04-13 20:42:32 +0000

    Task 3 - malloc

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * hash_table_set - adds an element to the hash table
@@ -11,15 +12,25 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
     unsigned long int index;
+    hash_node_t *node;
+
+    /*allocate memory */
+    node = malloc(sizeof(hash_node_t *));
+    if (node == NULL)
+        return (0);
+
     /* get index to store value based of hashed key */
     index = key_index((unsigned char *)key, ht->size);
     /* if the value at a specific index is empty */
     if (ht->array[index] == NULL)
     {
         /* set the value */
-        printf("hello\n");
-        ht->array[index].key = value;
-        ht->array[index].next = NULL;
+        /* printf("%s\n", (char *)value);*/
+        /* assign the respective values */
+        node->key = strdup(key);
+        node->value = strdup(value);
+        node->next = NULL;
+        ht->array[index] = node;
     }
     
     return 1;
