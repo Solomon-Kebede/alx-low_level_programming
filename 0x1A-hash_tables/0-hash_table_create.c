832577f747c80cf6763ba0960f8d2ca36b86f8c5
Solomnon Kebede <solomnonaau188@gmail.com>
2023-04-13 11:32:09 +0000

    Task 0 - Add docstrings

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,14 @@
 #include "hash_tables.h"
 
-hash_table_t *hash_table_create(unsigned long int size) {
+/**
+ * hash_table_create - create a new hashtable
+ * @size: integer size of the array
+ *
+ * Return: new array
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
 	/* declare a new hashtable */
 	hash_table_t *new_array;
 
